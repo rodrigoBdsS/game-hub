97e524cc7eb85296dfed5e2546e9795824780640
rodri <rodri@DESKTOP-OKSA0RK>
2023-06-07 19:32:00 -0300

    Building search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -10,8 +10,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
