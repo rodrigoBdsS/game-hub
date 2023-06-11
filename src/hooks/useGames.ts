4270f805ca2e6d2dd230ce8f9ebd261359c0eb05
rodri <rodri@DESKTOP-OKSA0RK>
2023-06-11 16:09:49 -0300

    Fetching game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,8 +9,10 @@ const apiClient = new APIClient<Game>('/games')
 export interface Game {
   id: number;
   name: string;
-  background_image: string,
-  parent_platforms: { platform: Platform }[],
+  slug: string;
+  description_raw: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
 }
