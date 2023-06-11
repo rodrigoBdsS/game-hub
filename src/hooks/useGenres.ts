f78a2bfc2a29e0750cbd09a4ecb688d5c6b8dee8
rodri <rodri@DESKTOP-OKSA0RK>
2023-06-11 16:34:08 -0300

    Refactoring entities

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,14 +3,9 @@ import APIClient from '../services/apiClient';
 
 import { useQuery } from '@tanstack/react-query';
 import ms from 'ms';
+import { Genre } from '../entities/Genre';
 
 const apiClient = new APIClient<Genre>('/genres');
-
-export interface Genre {
-  id: number,
-  name: string,
-  image_background: string
-}
 
 const useGenres = () => 
   useQuery({
